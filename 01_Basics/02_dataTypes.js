a310e2034ed907b36cc58dc24b7b63a7c0dd1832
Ashish Gupta <151548619+guptashish07@users.noreply.github.com>
2024-03-20 22:08:33 +0000

    data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all JS code as newer version
+
+
+// alert("Hello") //We are using nodejs, not browser
+
+//number, bigint, string, boolean, null=> standalone value, undefined
+// symbol => unique
+//object
+
+let name = "Ashish"
+let age = 25
+let isLoggedIn = true
+let state = null
+let email;
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof email)
