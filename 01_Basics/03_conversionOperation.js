244f6b17724e7d8b188916d0d48638aabceff321
Ashish Gupta <151548619+guptashish07@users.noreply.github.com>
2024-03-25 17:59:43 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -49,3 +49,39 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//********************Operations************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " ashish"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+//console.log(true+);  //returns error 
+
+console.log(+""); // returns 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+
+let gameCounter = 100
+gameCounter++; //postfix
+console.log(gameCounter);
+
+let gameCounter2 = 110
+++gameCounter2;  // ptefix
+console.log(gameCounter2);
+
