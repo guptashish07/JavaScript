3144bc3c856d404844e524384c0610bec010678b
Ashish Gupta <151548619+guptashish07@users.noreply.github.com>
2024-03-27 18:25:39 +0000

    primitive data

--- 01_Basics/05_dataTypes_final.js
+++ 01_Basics/05_dataTypes_final.js
@@ -1,1 +1,21 @@
 // Primitive dataTypes
+// 7 types: String, Number, Boolean, null, undefined, Symbol, Bigint
+
+const score = 100
+const scoreValue = 10.35
+const isLoggedIn = false
+const outSideTemprature = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 324578641252428542n
+
+
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+
