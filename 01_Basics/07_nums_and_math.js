35f48366f3a1b1763afa2201530bb2570fffd323
Ashish Gupta <151548619+guptashish07@users.noreply.github.com>
2024-03-29 19:01:08 +0000

    2nd commit

--- 01_Basics/07_nums_and_math.js
+++ 01_Basics/07_nums_and_math.js
@@ -22,4 +22,28 @@ console.log(hundreds.toLocaleString('en-IN'));
 
 
 const hundreds2 = 10000000000
-console.log(hundreds2.toLocaleString('en-IN'));
+console.log(hundreds2.toLocaleString());
+
+
+
+//***************************************************************************************
+
+console.log(Math);
+
+console.log(Math.abs(-8));
+console.log(Math.abs(8));
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.8));
+console.log(Math.min(434,364,21,242,47,254,425,564,8,576,45,67));
+console.log(Math.max(434,364,21,242,47,254,425,564,8,576,45,67));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
