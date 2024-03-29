adab9b920977fcc3b722a9709553eb3efa6dc9cc
Ashish Gupta <151548619+guptashish07@users.noreply.github.com>
2024-03-29 18:26:23 +0000

    first commit about numbers

---
+++ 01_Basics/07_nums_and_math.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString.length);
+
+console.log(typeof(balance.toString()));
+console.log(typeof balance);
+
+console.log(balance.toFixed(1));
+
+const otherName = 123.22546
+
+console.log(otherName.toPrecision(3));
+console.log(otherName.toPrecision(2));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
