46bc74abe0a2567f05ab8b69fa7974776b8769ba
Ashish Gupta <151548619+guptashish07@users.noreply.github.com>
2024-03-31 21:59:16 +0000

    scove level and mini hoisting

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -11,3 +11,53 @@ if(true){
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function one(){
+    const username = "Ashish"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);  // error 
+
+    two()
+}
+
+one()
+
+
+if(true){
+    const username = "Ashish"
+
+    if(username === "Ashish"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+addone(5)
+
+function addone(num){
+    return num + 1
+}
+
+
+// addTwo(7) //error accessing before declaration 
+
+const addTwo = function(num){
+    return num + 2
+}
+
+// hoisting => accessing before declaration gives error if function is contained in a variable 
+
