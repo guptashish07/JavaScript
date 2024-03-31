3b673245c2a74b45f44eabad7e6981d698fb4424
Ashish Gupta <151548619+guptashish07@users.noreply.github.com>
2024-03-31 19:15:28 +0000

    functions with objects and aaray

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -50,3 +50,44 @@ function loginUserMessage(userName = "sam"){
 }
 
 console.log(loginUserMessage());
+console.log(loginUserMessage("Ashish"));
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 600));
+
+
+
+function calculateCartPrice2(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice2(200, 400, 500, 600));
+
+const user = {
+    userName: "Ashish",
+    age: 25
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.userName} and age is ${anyObject.age}.`);
+}
+
+handleObject(user)
+
+handleObject({
+    userName: "Sam",
+    age:22
+})
+
+
+const myNewArray = [200, 100, 400, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([11, 50, 14, 125, 112, 12, 121, 251, 40]));
