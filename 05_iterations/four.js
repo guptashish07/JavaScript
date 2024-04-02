5c31321199302723a013633775d4889d03f051d9
Ashish Gupta <151548619+guptashish07@users.noreply.github.com>
2024-04-02 21:59:34 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,25 @@
+//for in
+
+const myObject ={
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(key);
+}
+for (const key in myObject) {
+    // console.log(myObject[key]);
+}
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
